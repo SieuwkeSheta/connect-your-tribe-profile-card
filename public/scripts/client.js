e42902f41ff0157b594299ba37bd6d97fd08b657
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2026-02-05 17:34:06 +0100

    Bestands naam 'script.js' veranderd naar 'client.js' #6

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -73,12 +73,3 @@ function showHobbies() {
   filterAmbitions.classList.remove('is-clicked')
   filterWork.classList.remove('is-clicked')
 }
-
-
-// Automatisch leeftijd aanpassen
-var dob = new Date("07/02/1998");
-var month_diff = Date.now() - dob.getTime();
-var age_dt = new Date(month_diff);
-var year = age_dt.getUTCFullYear();
-var age = Math.abs(year - 1970);
-document.getElementById("age").innerHTML = age;
